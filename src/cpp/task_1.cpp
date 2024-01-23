f443a963ef20f7ccec56607a81981b05feac6040
Dilbekhon <127374324+Dilbekhon@users.noreply.github.com>
2024-01-23 09:59:46 -0500

    Update task_1.cpp

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -4,6 +4,38 @@
  * Name:
  */
 
-class Problem1 {
-public:
-};
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findWaysToEvaluateToTarget(const vector<int>& nums, int target) {
+    int n = nums.size();
+    
+    vector<vector<int>> dp(n + 1, vector<int>(2 * target + 1, 0));
+
+    dp[0][target] = 1;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <= 2 * target; ++j) {
+            if (j - nums[i - 1] >= 0) {
+                dp[i][j] += dp[i - 1][j - nums[i - 1]];
+            }
+            if (j + nums[i - 1] <= 2 * target) {
+                dp[i][j] += dp[i - 1][j + nums[i - 1]];
+            }
+        }
+    }
+
+    return dp[n][target];
+}
+
+int main() {
+    vector<int> nums = {1, 1, 1, 1, 1};
+    int target = 3;
+
+    int result = findWaysToEvaluateToTarget(nums, target);
+    cout << result << endl;
+
+    return 0;
+}
