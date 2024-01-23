b59d510bee776f7bd5bb9596c2d642db5bf52ea7
Dilbekhon <127374324+Dilbekhon@users.noreply.github.com>
2024-01-23 10:17:13 -0500

    Update task_6.cpp

--- src/cpp/task_6.cpp
+++ src/cpp/task_6.cpp
@@ -4,6 +4,32 @@
  * Name:
  */
 
-class Problem6 {
-public:
-};
+#include <iostream>
+
+bool isPrime(int n) {
+  if (n <= 1) return false;
+  for (int i = 2; i * i <= n; ++i) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+int main() {
+  int n;
+  std::cout << "Enter a number: ";
+  std::cin >> n;
+
+  int nextPrime = n + 1;
+  while (!isPrime(nextPrime)) {
+    ++nextPrime;
+  }
+
+  int nextNextPrime = nextPrime + 1;
+  while (!isPrime(nextNextPrime)) {
+    ++nextNextPrime;
+  }
+
+  std::cout << "The next two prime numbers greater than " << n << " are: " << nextPrime << " and " << nextNextPrime << std::endl;
+
+  return 0;
+}
